1510c284a131fe46703802c0c2d531cf075f6270
vanchonlee <vanchonlee@gmail.com>
2019-07-22 10:55:55 +0700

    them delegated_tenant_id

---
+++ monasca_setup/service/__init__.py
@@ -0,0 +1,5 @@
+"""Setup system service for monasca-agent and its dependencies.
+
+Configure systemd to start monasca-collector, monasca-forwarder,
+and monasca-statsd services using a target named monasca-agent.
+"""
